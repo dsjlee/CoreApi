597398e7ece11bb82c062b29637dfd7c20fa6cfc
dsjlee <>
2018-09-06 21:16:03 -0700

    init

---
+++ CoreApi/Controllers/CoinController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowLocalhost")]
+    [ApiController]
+    public class CoinController : ControllerBase
+    {
+        private readonly IMemoryCache _cache;
+        private readonly IHttpClientFactory _clientFactory;
+        private static bool isCoinListCaching = false;
+        private static bool isGlobalMetricsCaching = false;
+
+        public CoinController(IMemoryCache memoryCache, IHttpClientFactory clientFactory)
+        {
+            _cache = memoryCache;
+            _clientFactory = clientFactory;
+        }
+
+        // GET: api/Coin
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var cacheKey = "CoinList";
+
+            // Look for cache key.
+            if (!_cache.TryGetValue(cacheKey, out string cacheEntry) && !isCoinListCaching)
+            {
+                // Key not in cache, so get data.
+                isCoinListCaching = true;
+                var client = _clientFactory.CreateClient("CoinMarketCap"); // Startup.cs -> ConfigureServices
+                HttpResponseMessage response = await client.GetAsync("v1/cryptocurrency/listings/latest");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    cacheEntry = await response.Content.ReadAsStringAsync();
+                    CacheInMemory(cacheKey, cacheEntry);
+                }
+                isCoinListCaching = false;
+            }
+            
+            return cacheEntry;
+        }
+
+        [Route("~/api/[controller]/metrics")]
+        [HttpGet]
+        public async Task<string> Metrics()
+        {
+            var cacheKey = "GlobalMetrics";
+
+            // Look for cache key.
+            if (!_cache.TryGetValue(cacheKey, out string cacheEntry) && !isGlobalMetricsCaching)
+            {
+                // Key not in cache, so get data.
+                isGlobalMetricsCaching = true;
+                var client = _clientFactory.CreateClient("CoinMarketCap"); // Startup.cs -> ConfigureServices
+                HttpResponseMessage response = await client.GetAsync("v1/global-metrics/quotes/latest ");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    cacheEntry = await response.Content.ReadAsStringAsync();
+                    CacheInMemory(cacheKey, cacheEntry);
+                }
+                isGlobalMetricsCaching = false;
+            }
+
+            return cacheEntry;
+        }
+
+        [NonAction]
+        private void CacheInMemory(string cacheKey, string cacheEntry)
+        {
+            var cacheEntryOptions = new MemoryCacheEntryOptions()
+                        .SetAbsoluteExpiration(TimeSpan.FromMinutes(5));
+            //.RegisterPostEvictionCallback(callback: EvictionCallback, state: this);
+            _cache.Set(cacheKey, cacheEntry, cacheEntryOptions);
+            System.Diagnostics.Debug.WriteLine($"Cached {cacheKey}: {DateTime.Now}");
+        }
+
+        //[NonAction]
+        //private static void EvictionCallback(object key, object value, EvictionReason reason, object state)
+        //{
+
+        //}
+
+        // GET: api/Coin/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Coin
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Coin/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CoreApi/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace CoreApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMemoryCache();
+
+            services.AddHttpClient("CoinMarketCap", client => 
+            {
+                client.BaseAddress = new Uri("https://pro-api.coinmarketcap.com/"); // can't change once it's set
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("X-CMC_PRO_API_KEY", "");
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowLocalhost",
+                    builder => builder
+                    .WithOrigins("http://localhost:4200", "https://dsjlee.bitbucket.io", "https://dsjlee.github.io")
+                    //.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    //.AllowCredentials()
+                );
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseCors("AllowLocalhost");
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
